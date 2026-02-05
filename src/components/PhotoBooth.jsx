4742523ea5d946938fe6e19a3c667478234f1ae1
Jatin <jatinverma2428@gmail.com>
2026-02-05 11:45:09 +0530

    change

--- src/components/PhotoBooth.jsx
+++ src/components/PhotoBooth.jsx
@@ -24,7 +24,7 @@ const PhotoBooth = () => {
 
   return (
     <div className="booth-container">
-      <div className="booth-header">Dv PhotoBooth</div>
+      <div className="booth-header">PhotoBooth</div>
 
       <div className="booth-body">
         <div className="coin-slot">
